328a921c3414b846e89a438d0c45e2d30fc32d12
RobertCarson <x00089687@ittd.ie>
2014-10-24 11:13:08 +0100

    added diary script for week 6

---
+++ week6/diary.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#: Title : 
+#: Date : 
+#: Author : 
+#: Version :
+#: Description : 
+#: Options : 
+
+
+echo "Please enter the day of the week"
+read input_variable1
+echo "The day you entered is: $input_variable1"
+
+the date = $( date )
+
+printf " %s %s " "$thedate" 
